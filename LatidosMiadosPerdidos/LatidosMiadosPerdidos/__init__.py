59c19f91e98a16fa50faa2164b0fc36e93198a93
Nilo-Univesp <2102237@aluno.univesp.com>
2023-05-12 16:56:42 -0300

    Update init.py

--- LatidosMiadosPerdidos/LatidosMiadosPerdidos/__init__.py
+++ LatidosMiadosPerdidos/LatidosMiadosPerdidos/__init__.py
@@ -2,10 +2,9 @@
 The flask application package.
 """
 
-import os
 from flask import Flask
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
-app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static')
+app.config['UPLOAD_FOLDER'] = '/tmp'
 
 import LatidosMiadosPerdidos.views
